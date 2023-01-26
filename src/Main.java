dae96742a524b46c92f317992e0804cf3305eb23
PupilCurious <ertugrul.bilal78@gmail.com>
2023-01-26 11:12:16 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        int studentNumber = 21;
+        String message = "Students Number : ";
+        System.out.println(studentNumber + message);
+    }
+}
